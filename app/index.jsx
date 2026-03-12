3249a7652805bed16f89b14de3e85458c861e053
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2026-03-11 22:05:32 -0300

    projeto do relogio em sala

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,19 @@
-import { Text, View,StyleSheet } from "react-native";
+import { Text, View,StyleSheet, Pressable, Image } from "react-native";
 
 export default function Index() {
   return (
     <View style = {style.container}>
-      <Text>Bora Bill</Text>
+        <Image source={require('./unnamed.png')} />
+        <View style={style.actions}>
+        <Text style={style.timer}>25:00</Text>
+        <Pressable style={style.button}>
+            <Text style={style.textButton}>Começar</Text>
+        </Pressable>
+    </View>
+    <View style={style.footer}>
+        <Text style={style.textfooter}>Curso de react native </Text>
+        <Text style={style.textfooter}> 2025 - Meu App </Text>
+    </View>
     </View>
   );
 }
@@ -11,6 +21,49 @@ export default function Index() {
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#021129"
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+        gap: 40
+    },
+    actions:{
+        padding: 24,
+        backgroundColor: "#14448080",
+        width: "80%",
+        borderRadius: 32,
+        borderWidth: 2,
+        borderColor: "#144480",
+        gap: 32
+    },
+    timer: {
+        fontSize: 54,
+        color: "#fff",
+        fontWeigth: "bold",
+        textAlign: "center"
+    },
+    image: {
+        width: 300,
+        height: 300,
+        resizeMode: "contain"
+    },
+    button:{
+        backgroundColor: 'B872FF',
+        borderRadius: 32,
+        padding: 8
+    },
+    textButton:{
+        textAlign: "center",
+        color: "#021123",
+        fontSize: 18,
+    },
+    footer:{
+        width: "80%",
+        backgroundColor: "black"
+    },
+    textfooter:{
+        textAlign: "center",
+        color: "#98A0A8",
+        fontStyle: 12.5
     }
+
 })
